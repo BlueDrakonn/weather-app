1339c2506787378d52b92fa8e1c3900b41174d88
BlueDrakon <kystiklistik228@gmail.com>
2025-04-27 17:35:41 +0300

    при старте приложения выполняется запрос к апи, пока это делается просто внутри composable, далее будет вынесено в репозиторий, была цель проверить работоспособность методов апи

--- app/src/main/java/com/example/wheatherapp/MainActivity.kt
+++ app/src/main/java/com/example/wheatherapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.wheatherapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,9 +10,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.wheatherapp.ui.theme.WheatherAppTheme
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +44,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var text_weather by remember { mutableStateOf("") }
+    val API_KEY = "4fdea4d317c64940b3f130345252502"
+
+    val latitude = 54
+    val longitude = 54
+    //"http://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$latitude,$longitude"
+    val client = OkHttpClient()
+    val request = Request.Builder()
+        .url("https://api.weatherapi.com/v1/current.json?key=4fdea4d317c64940b3f130345252502&q=London&aqi=no")
+        .build()
+
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {
+            Log.d("EXSE", "$e")
+            text_weather = "ошибка $e"
+
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            response.use { response ->
+
+                if (!response.isSuccessful) text_weather ="неудачный запрос" else text_weather =
+                    response.body?.string().toString()
+
+            }
+        }
+
+    })
+
     Text(
-        text = "Hello $name!",
+        text = "$text_weather",
         modifier = modifier
     )
 }
